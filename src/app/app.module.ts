3730ed7011ee03567e0d846d9e06829b13a0d565
mdattu <dattu.athreya7788@gmail.com>
2019-01-08 20:26:30 +1100

    Added introduction component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule
