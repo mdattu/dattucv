e6603fe94903c3304e9c222659916296db5e9cfb
mdattu <dattu.athreya7788@gmail.com>
2019-01-08 20:36:09 +1100

    Added education component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { EducationComponent } from './education/education.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroductionComponent
+    IntroductionComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule
